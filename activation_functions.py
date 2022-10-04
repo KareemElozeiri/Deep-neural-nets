69df50638ae6f9a368b9c9580d1468b3ed4c91f8
KareemElozeiri <k.elozeiri@gmail.com>
2022-10-04 17:06:19 +0200

    added some activation functions

---
+++ activation_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def step(x)->int:
+	if x<=0:
+		return 0
+	return 1
+
+def sigmoid(x)->foat:
+	return 1/(1+np.exp(-x))
+
+def relu(x)->float:
+	return max(0,x)
+
+def tanh(x)->float:
+	return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+
